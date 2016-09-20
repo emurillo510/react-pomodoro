89e021051c93a6e5cbce543f7dca8ceb19f755d1
Elton Murillo <emurillo510@gmail.com>
2016-09-19 20:27:10 -0700

    Adds src/PomodoroTimerInitializer.js, src/PomodoroTimerClock.js,

---
+++ src/PomodoroTimerClock.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class PomodoroTimerClock extends Component {
+
+  render() {
+
+    return (
+
+      <div id="pomodoro-timer-clock">
+        <p>08:35</p> 
+      </div>
+    
+    );
+
+  }
+
+}
+
+export default PomodoroTimerClock;
+
---
+++ src/PomodoroTimerInitializer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class PomodoroTimerInitializer extends Component {
+
+  render() {
+
+
+    return (
+
+      <div id="pomodoro-timer-initializer">
+
+        <button>pomodoro</button>
+        <button>short break</button>
+        <button>long break</button>
+      
+      </div>
+    
+    );
+
+  }
+
+}
+
+export default PomodoroTimerInitializer;
