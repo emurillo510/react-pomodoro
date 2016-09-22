2bc1e5995f58fa8e59a71a20601949f1fdad6733
Elton Murillo <emurillo510@gmail.com>
2016-09-21 20:07:47 -0700

    Implements countdown feature

--- src/PomodoroTimerClock.js
+++ src/PomodoroTimerClock.js
@@ -4,17 +4,34 @@ class PomodoroTimerClock extends Component {
 
   constructor(props) {
     super(props);
+    this.updateTimer = this.updateTimer.bind(this);
+
+  }
+
+  updateTimer() {
+    this.props.updateTime();
   }
 
   render() {
 
-    let min = this.props.initialTime / 60;
-    let seconds = this.props.initialTime % 60;
+    switch(this.props.status) {
+      case "STOPPED":
+      break;
+      case "RUNNING":
+      clearInterval(this.interval);
+      this.interval = setInterval(this.updateTimer, 1000);
+      break;
+      case "RESET":
+      break;
+      default:
+
+    }
 
     return (
 
       <div id="pomodoro-timer-clock">
-        <p>{min}:{seconds}</p>
+        <p>{this.props.status}</p>
+        <p>{this.props.time}</p>
       </div>
     
     );
